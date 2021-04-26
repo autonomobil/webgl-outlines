12975ec934655e0fc36a62c4b4cc2f068f3e5ad5
Omar Shehata <omar.sameh.shehata@gmail.com>
2021-04-25 21:02:17 -0400

    Add Sketchfab integration

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -8,8 +8,13 @@ import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader.js";
 
 import { CustomOutlinePass } from "./CustomOutlinePass.js";
 import DragAndDropModels from "./DragAndDropModels.js";
+import SketchfabIntegration from "./SketchfabIntegration.js";
 
 const GUI = dat.GUI;
+
+const sketchfabIntegration = new SketchfabIntegration();
+sketchfabIntegration.checkToken();
+
 
 // Init scene
 const camera = new THREE.PerspectiveCamera(
@@ -27,6 +32,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const light = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(light);
 light.position.set(1.7, 1, -1);
+
 
 // Set up post processing
 // Create a render target that holds a depthTexture so we can use it in the outline pass
@@ -140,3 +146,27 @@ gui.add(params, "normalMult", 0.0, 10).onChange(function (value) {
 // Allow drag and drop models to visualize them with outlines
 const dropZoneElement = document.querySelector("body");
 DragAndDropModels(scene, dropZoneElement);
+
+
+// Add Sketchfab integration buttons to GUI
+const sketchfabFolder = gui.addFolder('Sketchfab');
+const sfParams = {
+  "Sketchfab URL": '',
+};
+
+let loginButtonName = 'Login to Sketchfab';
+if (sketchfabIntegration.token != null) {
+  let lastValue;
+  sketchfabFolder.add(sfParams, 'Sketchfab URL').onChange(async function (value) {
+    if (lastValue != value) {
+      lastValue = value;
+      sketchfabIntegration.fetchAndDisplayModel(value, scene);
+    }
+  });
+  loginButtonName = 'Re-login to Sketchfab';
+}
+
+const sketchfabOptions = {};
+sketchfabOptions[loginButtonName] = sketchfabIntegration.authenticate;
+sketchfabFolder.add(sketchfabOptions, loginButtonName);
+sketchfabFolder.open();
